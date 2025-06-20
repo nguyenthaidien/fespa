13259deb391ce00f175137e2b5536c25f91c486b
nguyenthaidien <45583973+nguyenthaidien@users.noreply.github.com>
2025-06-20 08:53:03 +0700

    Update code

--- dcms/src/environments/environment.ts
+++ dcms/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // environment.ts (Development)
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:9090/api',
+    apiUrl: 'https://localhost:9090/api',
     featureFlag: true,
     keycloak: {
       url: 'https://10.82.14.80:8443',
